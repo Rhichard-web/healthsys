6109e8325b9b759ccdc5593b6f9cb918122d4c60
Chan <845841840@qq.com>
2023-12-14 23:06:44 +0800

    第一次提交到本地仓库

---
+++ src/test/java/cn/chan/healthsys/HealthsysApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chan.healthsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
